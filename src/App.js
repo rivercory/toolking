527bba0fd3e7fcb50b49cde65ce580cea2105a3d
hyeonho2010 <hyeonhokang10@kakao.com>
2024-01-07 11:20:15 +0900

    Version 1.2.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {Link, Route, Routes} from 'react-router-dom';
-import {HiMenu, HiSearch, HiMail} from "react-icons/hi";
+import { Link, Route, Routes } from 'react-router-dom';
+import { HiMenu, HiSearch, HiMail } from "react-icons/hi";
 import './App.css';
 import Home from './pages/Home';
 import About from "./pages/About";
@@ -7,12 +7,12 @@ import List from "./pages/List";
 
 function App() {
     return (
-        <div className="relative">
-            <div className="navbar bg-base-100 hidden md:block">
+        <div>
+            <div className="w-auto navbar m-4 rounded-full border-2 bg-base-100 hidden md:block">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                            <HiMenu size={32}/>
+                            <HiMenu size={32} />
                         </div>
                         <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 suit">
                             <li className="menu-title">툴킹</li>
@@ -27,47 +27,47 @@ function App() {
                             <li><a href="https://toolking.vercel.app">툴킹</a></li>
                         </ul>
                     </div>
-                    <Link to="/" className="btn btn-ghost text-xl">
-                        <img src="./icons/toolking_transparent.svg" width="32" height="32"/>
+                    <Link to="/" className="btn btn-ghost text-2xl pretendard">
+                        <img src="./icons/toolking_transparent.svg" width="32" height="32" />
                         툴킹
                     </Link>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
-                        <HiSearch size={32}/>
+                        <HiSearch size={32} />
                     </button>
                     <button className="btn btn-ghost btn-circle">
-                        <HiMail size={36}/>
+                        <HiMail size={36} />
                     </button>
                 </div>
             </div>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/list" element={<List/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/list" element={<List />} />
             </Routes>
             <div className="btm-nav md:hidden">
                 <button>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
+                        stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
                     <span className="btm-nav-label">Home</span>
                 </button>
                 <button className="active">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
+                        stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     <span className="btm-nav-label">Warnings</span>
                 </button>
                 <button>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
+                        stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
+                            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                     </svg>
                     <span className="btm-nav-label">Statics</span>
                 </button>
